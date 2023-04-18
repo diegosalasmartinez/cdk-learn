085c46af096d449dc88609aeb485a557ddb8cc1f
Diego Salas <Diego.Salas@promart.pe>
2023-04-17 20:01:32 -0500

    Create a docker image and deploy it through a load balancer

--- hello-world/lib/hello-world-stack.ts
+++ hello-world/lib/hello-world-stack.ts
@@ -1,14 +1,37 @@
 import * as cdk from 'aws-cdk-lib';
+import {
+  Container,
+  Environment,
+  HttpLoadBalancerExtension,
+  Service,
+  ServiceDescription
+} from '@aws-cdk-containers/ecs-service-extensions';
+import { Platform } from 'aws-cdk-lib/aws-ecr-assets';
 import { Construct } from 'constructs';
 
 export class HelloWorldStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new cdk.aws_s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true
+    const environment = new Environment(this, 'production');
+
+    const serviceDescription = new ServiceDescription();
+
+    // Add the container
+    serviceDescription.add(new Container({
+      cpu: 1024,
+      memoryMiB: 2048,
+      trafficPort: 80,
+      image: cdk.aws_ecs.ContainerImage.fromAsset('app', {platform:Platform.LINUX_AMD64,})
+    }));
+
+    // Add a load balancer
+    serviceDescription.add(new HttpLoadBalancerExtension());
+
+    // Add the service to the production environment.
+    new Service(this, 'docker-demo', {
+      environment: environment,
+      serviceDescription: serviceDescription,
     });
   }
 }
